783bc1ca05266f6ecf2332296468a8fa91b6359c
Brittany Diesbourg <bdiesbourg@mypartyalbum.com>
2022-01-26 13:15:27 -0500

    Created solutions for client and library, added project reference, created basic empty files to start with

---
+++ QuiddlerLibrary/QuiddlerClient/Program.cs
@@ -0,0 +1,14 @@
+﻿// Brittany Diesbourg & Dianne Corpuz - Section A
+
+using System;
+
+namespace QuiddlerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ QuiddlerLibrary/QuiddlerLibrary/Deck.cs
@@ -0,0 +1,14 @@
+﻿// Brittany Diesbourg & Dianne Corpuz - Section A
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiddlerLibrary
+{
+    class Deck
+    {
+    }
+}
