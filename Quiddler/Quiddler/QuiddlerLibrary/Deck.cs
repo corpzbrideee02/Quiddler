c6e685c8aeb8d3894dc9b8fb95538f15af2ef58f
Dianne <corpuzdianne0302@gmail.com>
2022-02-02 15:20:45 -0500

    updated TopDiscard, added stack of discardedCards, added useful methods

--- Quiddler/Quiddler/QuiddlerLibrary/Deck.cs
+++ Quiddler/Quiddler/QuiddlerLibrary/Deck.cs
@@ -15,26 +15,28 @@ namespace QuiddlerLibrary
         private const int NumberOfCards = 118;
         private static int Twelve = 12, Ten = 10, Eight = 8, Six = 6, Four = 4, Two = 2;
 
-        //
-        private List<IPlayer> players = null;
-        private int undealtCards;
+        private int undealtCards=118;
 
         //c'tor 
-        /*public Deck()
+        public Deck()
         {
-
-        }*/
+            ShuffleCardsInDeck();
+        }
 
 
         public string About => $"Test Client for: {GetType().Namespace}, Developers: Brittany Diesbourg and Dianne Corpuz";
 
-        private Dictionary<string, int> CardCountsInDeck = new Dictionary<string, int>()
-        {
-                {"a",Ten}, {"b",Two},  {"c",Two},{"d",Four}, {"e",Twelve},{"f",Two},{"g",Four}, {"h",Two},{"i",Eight},  {"j",Two},  {"k",Two},  {"l",Four}, {"m",Two},{"n",Six}, {"o",Eight},
-                 {"p",Two}, {"q",Two},{"r",Six},{"s",Four},{"t",Six},{"u",Six}, {"v",Two}, {"w",Two}, {"x",Two},  {"y",Four}, {"z",Two},  {"cl",Two}, {"er",Two}, {"in",Two},{"qu",Two}, {"th",Two},
-        };
+        private Dictionary<string, int> CardsInDeck = new Dictionary<string, int>()
+         {
+                 {"a",Ten}, {"b",Two},  {"c",Two},{"d",Four}, {"e",Twelve},{"f",Two},{"g",Four}, {"h",Two},{"i",Eight},  {"j",Two},  {"k",Two},  {"l",Four}, {"m",Two},{"n",Six}, {"o",Eight},
+                  {"p",Two}, {"q",Two},{"r",Six},{"s",Four},{"t",Six},{"u",Six}, {"v",Two}, {"w",Two}, {"x",Two},  {"y",Four}, {"z",Two},  {"cl",Two}, {"er",Two}, {"in",Two},{"qu",Two}, {"th",Two},
+         };
 
 
+        //stack discarded Cards
+        private Stack<string> DiscardedCards = new Stack<string>();
+
+        
         public int CardCount
         {
             get
@@ -69,50 +71,47 @@ namespace QuiddlerLibrary
             }
         }
 
-        
+        //if user wants the top discarded card, it will just gets it from the myStack(stack of discarded card)
+        //if not, it will randomly gets the card from the CardsInDeck(Dictionary)
         public string TopDiscard
         {
             get
             {
-                var random_ = new Random();
-                int index = random_.Next(CardCountsInDeck.Count());  //index of random picked items
-
-                var itemsPicked = "";
-                if (CardCountsInDeck.ElementAt(index).Value!=0)
+                if(TopDiscard==null)
                 {
+                    var random_ = new Random();
+                    int index = random_.Next(CardsInDeck.Count());  //index of random picked items
 
-                    itemsPicked = CardCountsInDeck.ElementAt(index).Key;
-                    CardCountsInDeck[CardCountsInDeck.ElementAt(index).Key] = CardCountsInDeck.ElementAt(index).Value - 1;
+                    var itemsPicked = "";
+                    if (CardsInDeck.ElementAt(index).Value > 0)
+                    {
 
+                        itemsPicked = CardsInDeck.ElementAt(index).Key;
+                        CardsInDeck[CardsInDeck.ElementAt(index).Key] = CardsInDeck.ElementAt(index).Value - 1;
+                        DiscardedCards.Push(itemsPicked);
+                    }
+
+                    --undealtCards;
+                    return itemsPicked;
                 }
 
-                return itemsPicked;
+                
+                --undealtCards;
+                return DiscardedCards.Pop();
+                
             }
         }
-
-
-
-        //Pick a random card from the deck
-        public string GetRandomCard()
-        {
-                var random_ = new Random();
-                int index = random_.Next(CardCountsInDeck.Count());  //index of card picked by random generator
-
-                    var itemsPicked =  CardCountsInDeck.ElementAt(index).Key;
-                    
-
-             return itemsPicked;
-        }
-
-
-
 
         IPlayer IDeck.NewPlayer()
         {
             //create new player, populates it with CardsPerPlayer cards
-
             IPlayer newPlayer = new Player(this);
-            players.Add(newPlayer);
+
+            for (int i=0; i<CardsPerPlayer;++i)
+            {
+                newPlayer.DrawCard();
+                --undealtCards;
+            }
 
             return newPlayer;
         }
@@ -120,11 +119,57 @@ namespace QuiddlerLibrary
         public override string ToString()
         {
             string deckInitialized = "";
-            foreach (var cards in CardCountsInDeck)
+            foreach (var cards in CardsInDeck)
             {
                 deckInitialized += $"{cards.Key}({cards.Value}) ";
             }
             return deckInitialized;
         }
+
+
+
+        //*************************************NoTE: just added these methods... Please disregard   *************************************
+
+        public Dictionary<string, int> GetCardsInDeck()
+        {
+            return CardsInDeck;
+        }
+
+
+        private void ShuffleCardsInDeck()
+        {
+            List<string> ShuffledCardsList = new List<string>();
+            foreach (var cards in CardsInDeck)
+            {
+                for (int i = 0; i < cards.Value; ++i)
+                {
+                    ShuffledCardsList.Add(cards.Key);
+                }
+            }
+
+            ShuffledCardsList = ShuffledCardsList.OrderBy(item => Guid.NewGuid()).ToList();
+        }
+
+        //draw pile
+        private Stack<string> DrawPile = new Stack<string>();
+
+        //Pick a random card from the deck
+        private string GetRandomCard()
+        {
+            var random_ = new Random();
+            int index = random_.Next(CardsInDeck.Count());  //index of random picked items
+
+            var itemsPicked = "";
+            if (CardsInDeck.ElementAt(index).Value != 0)
+            {
+                itemsPicked = CardsInDeck.ElementAt(index).Key;
+                CardsInDeck[CardsInDeck.ElementAt(index).Key] = CardsInDeck.ElementAt(index).Value - 1;
+            }
+
+            return itemsPicked;
+        }
+
+
+
     }
 }
