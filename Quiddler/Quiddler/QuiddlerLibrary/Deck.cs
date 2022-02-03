390f0d1a664c01184a49c86b71c7a5262c6f2407
Dianne <corpuzdianne0302@gmail.com>
2022-02-03 16:49:30 -0500

    Added CardPointValues, added getters and make ShuffleCardsInDeck public

--- Quiddler/Quiddler/QuiddlerLibrary/Deck.cs
+++ Quiddler/Quiddler/QuiddlerLibrary/Deck.cs
@@ -13,7 +13,7 @@ namespace QuiddlerLibrary
         // Private member variables
         private int cardsToBeDealt = 0;
         private const int NumberOfCards = 118;
-        private static int Twelve = 12, Ten = 10, Eight = 8, Six = 6, Four = 4, Two = 2;
+        //private static int Twelve = 12, Ten = 10, Eight = 8, Six = 6, Four = 4, Two = 2;
 
         private int undealtCards=118;
         private string topDiscard = "";
@@ -29,8 +29,14 @@ namespace QuiddlerLibrary
 
         private Dictionary<string, int> CardsInDeck = new Dictionary<string, int>()
          {
-                 {"a",Ten}, {"b",Two},  {"c",Two},{"d",Four}, {"e",Twelve},{"f",Two},{"g",Four}, {"h",Two},{"i",Eight},  {"j",Two},  {"k",Two},  {"l",Four}, {"m",Two},{"n",Six}, {"o",Eight},
-                  {"p",Two}, {"q",Two},{"r",Six},{"s",Four},{"t",Six},{"u",Six}, {"v",Two}, {"w",Two}, {"x",Two},  {"y",Four}, {"z",Two},  {"cl",Two}, {"er",Two}, {"in",Two},{"qu",Two}, {"th",Two},
+                 {"a",10}, {"b",2},  {"c",2},{"d",4}, {"e",12},{"f",2},{"g",4}, {"h",2},{"i",8},  {"j",2},  {"k",2},  {"l",4}, {"m",2},{"n",6}, {"o",8},
+                  {"p",2}, {"q",2},{"r",6},{"s",4},{"t",6},{"u",6}, {"v",2}, {"w",2}, {"x",2},  {"y",4}, {"z",2},  {"cl",2}, {"er",2}, {"in",2},{"qu",2}, {"th",2},
+         };
+
+        private Dictionary<string, int> CardPointValues = new Dictionary<string, int>()
+         {
+                 {"a",2}, {"b",8},  {"c",8},{"d",5}, {"e",2},{"f",6},{"g",6}, {"h",7},{"i",2},  {"j",13},  {"k",8},  {"l",3}, {"m",5},{"n",5}, {"o",2},
+                  {"p",6}, {"q",15},{"r",5},{"s",3},{"t",3},{"u",4}, {"v",11}, {"w",10}, {"x",12},  {"y",4}, {"z",14},  {"cl",10}, {"er",7}, {"in",7},{"qu",9}, {"th",9},
          };
 
 
@@ -125,13 +131,24 @@ namespace QuiddlerLibrary
 
         //*************************************NoTE: just added these methods... Please disregard   *************************************
 
-        public Dictionary<string, int> GetCardsInDeck()
+        public Dictionary<string, int> GetCardsInDeck
         {
-            return CardsInDeck;
+            get
+            {
+                return CardsInDeck;
+            }
+        }
+
+        public Dictionary<string, int> GetCardPointValues
+        {
+            get
+            {
+                return CardPointValues;
+            }
         }
 
 
-        private void ShuffleCardsInDeck()
+        public List<string> ShuffleCardsInDeck()
         {
             List<string> ShuffledCardsList = new List<string>();
             foreach (var cards in CardsInDeck)
@@ -143,6 +160,7 @@ namespace QuiddlerLibrary
             }
 
             ShuffledCardsList = ShuffledCardsList.OrderBy(item => Guid.NewGuid()).ToList();
+            return ShuffledCardsList;
         }
 
         //draw pile
